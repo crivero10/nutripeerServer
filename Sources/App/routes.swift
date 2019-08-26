394da4ccb697738b52771049109202eab6b38ca2
crivero10 <2700c.ri@gmail.com>
2019-08-26 00:01:28 -0500

    Inicial

---
+++ Sources/App/routes.swift
@@ -0,0 +1,25 @@
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    // Basic "It works" example
+    router.get { req -> String in
+        if let name = try? req.query.get(String.self, at:"name"){
+            return name
+        }
+        else{
+            return "No params"
+        }
+    }
+    
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello, world!"
+    }
+
+    // Example of configuring a controller
+    let todoController = TodoController()
+    router.get("todos", use: todoController.index)
+    router.post("todos", use: todoController.create)
+    router.delete("todos", Todo.parameter, use: todoController.delete)
+}
