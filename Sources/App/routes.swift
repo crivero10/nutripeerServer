2bbfa5c4b05df8515ea49b2e7d4920250484bea8
crivero10 <2700c.ri@gmail.com>
2019-08-28 02:18:40 -0500

    lil chanhge

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -18,12 +18,8 @@ public func routes(_ router: Router) throws {
     }
 
     // Example of configuring a controller
-    let todoController = TodoController()
     let personController = PersonController()
     
     router.post("person", use: personController.create)
     
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
