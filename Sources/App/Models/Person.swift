de66e9a6e8f1bc1754ce8bcf2c4cfb4fd9cb7425
crivero10 <2700c.ri@gmail.com>
2019-08-28 02:55:49 -0500

    y

--- Sources/App/Models/Person.swift
+++ Sources/App/Models/Person.swift
@@ -47,6 +47,17 @@ extension Person: Content { }
 extension Person: MySQLMigration { }
 
 func randomString(length: Int) -> String {
-  let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-  return String((0..<length).map{ _ in letters.randomElement()! })
+
+    let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    let len = UInt32(letters.length)
+
+    var randomString = ""
+
+    for _ in 0 ..< length {
+        let rand = arc4random_uniform(len)
+        var nextChar = letters.character(at: Int(rand))
+        randomString += NSString(characters: &nextChar, length: 1) as String
+    }
+
+    return randomString
 }
